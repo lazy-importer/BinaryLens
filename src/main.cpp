cca07bcfa4c18d8e87fb809ba34facc3b80180ba
Berk000x <168338177+Berk000x@users.noreply.github.com>
2025-10-11 19:20:16 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <windows.h>
+#include <iostream>
+
+#include "plugin/action_handler.h"
+
+#include <hexrays.hpp>
+
+__declspec(dllexport) plugin_t PLUGIN = {
+    IDP_INTERFACE_VERSION,
+    PLUGIN_PROC,
+    init,
+    term,
+    run,
+    "",
+    "",
+    "BinaryLens",
+    0
+};
